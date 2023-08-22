2dd948454fdcce30bc846142d4af9c623bc36d78
comodoroCampos <30672223+comodoroCampos@users.noreply.github.com>
2023-08-22 17:26:58 -0400

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,20 +5,22 @@ import { IonicModule } from '@ionic/angular';
 import { UserModel } from '../models/UserModel';
 import { NavigationExtras, Router, RouterLinkWithHref } from '@angular/router';
 import { IUserLogin } from '../models/IUserLogin';
+import { UsuarioPage } from '../usuario/usuario.page';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterLinkWithHref, FormsModule]})
+  imports: [IonicModule, CommonModule, RouterLinkWithHref, FormsModule]
+})
 export class LoginPage implements OnInit {
 
   listUser: UserModel[] = [
-    new UserModel('George','Campos','gcampos@gmail.com','USUARIO','gcampos','123456'),
-    new UserModel('Lucila','Julio','ljulio@gmail.com','ADMIN','ljulio','123456'),
-    new UserModel('Mickaella','Silva','msilva@gmail.com','USUARIO','msilva','123456'),
-    new UserModel('Juan','Soto','jsoto@gmail.com','ADMIN','jsoto','123456'),
+    new UserModel('George', 'Campos', 'gcampos@gmail.com', 'USUARIO', 'gcampos', '123456'),
+    new UserModel('Lucila', 'Julio', 'ljulio@gmail.com', 'ADMIN', 'ljulio', '123456'),
+    new UserModel('Mickaella', 'Silva', 'msilva@gmail.com', 'USUARIO', 'msilva', '123456'),
+    new UserModel('Juan', 'Soto', 'jsoto@gmail.com', 'ADMIN', 'jsoto', '123456'),
   ];
 
   userLoginModal: IUserLogin = {
@@ -32,19 +34,20 @@ export class LoginPage implements OnInit {
     this.userLoginModalRestart();
   }
 
-  userLogin(userLoginInfo: IUserLogin): boolean{
-    for(let i = 0; i < this.listUser.length; i++){
-      if((this.listUser[i].usuario == userLoginInfo.usuario) && (this.listUser[i].pass == userLoginInfo.pass)){
+  userLogin(userLoginInfo: IUserLogin): boolean {
+
+    for (let i = 0; i < this.listUser.length; i++) {
+      if ((this.listUser[i].usuario == userLoginInfo.usuario) && (this.listUser[i].pass == userLoginInfo.pass)) {
         console.log('User Loged...', this.userLoginModal.usuario, this.userLoginModal.pass);
         let userInfoSend: NavigationExtras = {
           state: {
             user: this.listUser[i]
           }
         }
-        if(this.listUser[i].tipo == 'USUARIO'){
+        if (this.listUser[i].tipo == 'USUARIO') {
           let sendInfo = this.route.navigate(['/usuario'], userInfoSend);
           return true;
-        }else{
+        } else {
           let sendInfo = this.route.navigate(['/admin'], userInfoSend);
           return true;
         }
@@ -52,10 +55,10 @@ export class LoginPage implements OnInit {
     }
     this.userLoginModalRestart();
     return false;
-    
+
   }
 
-  userLoginModalRestart(): void{
+  userLoginModalRestart(): void {
     this.userLoginModal.usuario = '';
     this.userLoginModal.pass = '';
   }
